9f063c30b16623d57e7b32563288097af91892c2
ssangjun <uio3001@dongguk.edu>
2022-12-02 15:56:10 +0900

    refactor: refactor review table string column size

--- src/main/java/PINAMO/FADEIN/data/Entity/ReviewEntity.java
+++ src/main/java/PINAMO/FADEIN/data/Entity/ReviewEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 public class ReviewEntity {
 
   @Id
+  @Column(columnDefinition = "VARCHAR(100)")
   private String id;
   @ManyToOne
   @JoinColumn(name ="userId", referencedColumnName = "id")
